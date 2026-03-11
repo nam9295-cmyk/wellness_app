ada46755637335007e2d891f9f10da67346fdee9
남형우 <nam9295@namhyeong-uui-MacBookAir.local>
2026-03-11 21:10:05 +0900

    Add energy fill animation

---
+++ app/+html.tsx
@@ -0,0 +1,20 @@
+import type { PropsWithChildren } from 'react';
+import { ScrollViewStyleReset } from 'expo-router/html';
+
+export default function RootHtml({ children }: PropsWithChildren) {
+  return (
+    <html lang="ko">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="theme-color" content="#EDC5C4" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
